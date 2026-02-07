06b027c5d73cd921381f1d48d75bd5b98d4931aa
Mariusz Maciejczuk <mariusz.maciejczuk@grandparade.co.uk>
2026-02-07 12:41:48 +0100

    Fix History page: use correct QuickGrid namespace

--- src/PetWorld.Infrastructure/obj/Debug/net8.0/PetWorld.Infrastructure.AssemblyInfo.cs
+++ src/PetWorld.Infrastructure/obj/Debug/net8.0/PetWorld.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetWorld.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1f94adb310b5613a0e028d278ff51054e4779a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5746e692062484b22ba897f4daf38e8bb65870e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetWorld.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetWorld.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
